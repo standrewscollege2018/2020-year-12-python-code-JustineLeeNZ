2b98e35a2a05376b8148c83fc3ab1c5d0316b8ea
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-02-14 15:31:32 +1300

    Added credit info functionality

--- credit_manager.py
+++ credit_manager.py
@@ -9,13 +9,18 @@ def display_all_students():
 
 
 # stores student details
-students = ["Justine Lee", "Bryn Lewis", "Meredith Lewis", "Rhys Lewis","Phil Adams"]
+students = [["Justine Lee",50], ["Bryn Lewis",20], ["Meredith Lewis",10], ["Rhys Lewis",5],["Phil Adams",100]]
+
 
 display_all_students()
 
     
 # add a student
-new_student = input("Enter the name of a student: ")
+student_name = input("Enter the name of a student: ")
+student_credit_total = int(input("Enter total NCEA level 1 credits"))
+
+new_student = [student_name, student_credit_total]
+
 students.append(new_student)
 
 display_all_students()
@@ -30,8 +35,10 @@ display_all_students()
 # get details of which student to change and what the updated name is
 student_num = int(input("Enter number of student to change: "))    
 new_name = input("Enter updated name: ")
+new_total = int(input("Enter new total: "))
 
 # change student details
-students[student_num-1] = new_name
+students[student_num-1][0] = new_name
+students[student_num-1][1] = new_total
 
 display_all_students()
